ca33fc3aa4f143b194d88cbefcba4d4420174489
Luan Simões <111541122+luanc179@users.noreply.github.com>
2023-05-24 15:15:08 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+"# cauteladearmt" 
+"# cauteladearmt" 
---
+++ src/test/java/com/armamento/cautela/CauteladeArmamento/CauteladeArmamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armamento.cautela.CauteladeArmamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CauteladeArmamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
